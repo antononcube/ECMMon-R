1d6d35f3c6c0a30d1369b4a9f01ab970e3d96908
antonocube <antononcube@gmail.com>
2020-04-12 13:29:02 -0400

    Implemented GetRates and GetStocks. Full working implementation of AddModelIdentifier.

--- R/EpidemiologyModelModifications.R
+++ R/EpidemiologyModelModifications.R
@@ -33,11 +33,26 @@ NULL
 #' @description Adds a specified identifier id to all stocks and rates on the model m.
 #' @param model A model object.
 #' @param id A number or string to used as ID.
+#' @param birthTermsQ A logical.
 #' @return A model.
-#' @family Epidemiology Model Modifiers
+#' @details The orginal model object has \code{birthTermsQ} in the closure of \code{model$RHSFunction}.
+#' (For now) it is easier to just have that parameter specified than imply it.
+#' @family Epidemiology Model Modifier functions
 #' @export
-AddModelIdentifier <- function( model, id ) {
+AddModelIdentifier <- function( model, id, birthTermsQ = FALSE ) {
 
+  if( !EpidemiologyFullModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( !( is.character(id) && length(id) == 1 ) ) {
+    stop( "The argument id is expected to be a string", call. = TRUE )
+  }
+
+  ## New model
+  newModel <- model
+
+  ## Mapping rules
   aNewStocks <- setNames( gsub( "([[:upper:]]+)t", paste0( "\\1_", id, "_t" ), names(model$Stocks) ), names(model$Stocks) )
 
   aNewStocksD <- setNames( paste0( "d", aNewStocks ), paste0( "d", names(model$Stocks) ) )
@@ -47,6 +62,27 @@ AddModelIdentifier <- function( model, id ) {
 
   aNewRates <- setNames( paste0( names(model$Rates), "_", id ), names(model$Rates) )
 
+  if( sum( names(aNewRates) %in% names(aNewStocks0) ) > 0 ) {
+
+    common <- intersect( names(aNewRates), names(aNewStocks0) )
+
+    aNewRates[ common ] <- aNewStocks0[ common ]
+
+  }
+
+  aBirthsTerm <- setNames( ifelse( birthTermsQ, "TRUE", "FALSE" ), c("birthsTermQ") )
+
+  ## Apply rules to stocks, rates, initial conditions, and rate rules
+  newModel$Stocks <- setNames( newModel$Stocks, aNewStocks[ names(newModel$Stocks) ] )
+
+  newModel$Rates <- setNames( newModel$Rates, aNewRates[ names(newModel$Rates) ] )
+
+  newModel$InitialConditions <- setNames( newModel$InitialConditions, aNewStocks[ names(newModel$InitialConditions) ] )
+
+  newModel$RateRules <- setNames( newModel$RateRules, aNewRates[ names(newModel$RateRules) ] )
+
+
+  ## Deparse RHS function and apply mapping rules
   lsRHS <- deparse(model$RHSFunction, control = "all" )
 
   lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocks)[[i]], "\\b" ), aNewStocks[[i]], txt ) }, init = lsRHS, x = 1:length(aNewStocks) )
@@ -57,5 +93,74 @@ AddModelIdentifier <- function( model, id ) {
 
   lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewRates)[[i]], "\\b" ), aNewRates[[i]], txt ) }, init = lsRHS, x = 1:length(aNewRates) )
 
-  lsRHS
+  lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aBirthsTerm)[[i]], "\\b" ), aBirthsTerm[[i]], txt ) }, init = lsRHS, x = 1:length(aBirthsTerm) )
+
+  newModel$RHSFunction <- eval( parse( text = lsRHS ) )
+
+  ## Result
+  newModel
+}
+
+
+##===========================================================
+## GetStocks
+##===========================================================
+
+#' Get stock symbols.
+#' @description Gets the stock symbols that have descriptions matching specified patterns.
+#' @param model A model object.
+#' @param patterns A a character vector of patterns to be applied to the descriptions of model's stocks.
+#' @return A character vector
+#' @family Epidemiology Model Query functions
+#' @export
+GetStocks <- function( model, patterns ) {
+
+  if( !EpidemiologyModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( ! is.character(patterns) ) {
+    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  }
+
+  lsAll <- model$Stocks
+
+  lsFocus <- purrr::map( patterns, function(ss) { grep( pattern = ss, x = lsAll, value = TRUE ) } )
+  lsFocus <- as.character( unlist( lsFocus ) )
+
+  names( model$Stocks[ model$Stocks %in% lsFocus  ] )
+
+}
+
+
+##===========================================================
+## GetRates
+##===========================================================
+
+#' Get rate symbols.
+#' @description Gets the rate symbols that have descriptions matching specified patterns.
+#' @param model A model object.
+#' @param patterns A a character vector of patterns to be applied to the descriptions of model's rates.
+#' @return A character vector
+#' @family Epidemiology Model Query functions
+#' @export
+GetRates <- function( model, patterns ) {
+
+  ## This code is very similar / same as GetStocks -- it has to be refactored.
+
+  if( !EpidemiologyModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( ! is.character(patterns) ) {
+    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  }
+
+  lsAll <- model$Rates
+
+  lsFocus <- purrr::map( patterns, function(ss) { grep( pattern = ss, x = lsAll, value = TRUE ) } )
+  lsFocus <- as.character( unlist( lsFocus ) )
+
+  names( model$Rates[ model$Rates %in% lsFocus  ] )
+
 }
