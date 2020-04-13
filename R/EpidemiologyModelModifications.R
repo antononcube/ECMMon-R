5c5941011505774b7b28503f1e31ce93a0fc980d
antonocube <antononcube@gmail.com>
2020-04-12 21:37:26 -0400

    Implemented model joining.

--- R/EpidemiologyModelModifications.R
+++ R/EpidemiologyModelModifications.R
@@ -29,17 +29,31 @@ NULL
 ## AddModelIdentifier
 ##===========================================================
 
+GetLeftHandSides <- function( definitionLines ) {
+
+  if( ! is.character(definitionLines) ) {
+    stop( "The argument definitionLines is expected to be a string", call. = TRUE )
+  }
+
+  pos <- grep( "\\s([[:alpha:]].*)\\s*<-(.*)", definitionLines )
+
+  lsOtherLHS <- gsub( "(\\s)([[:alpha:]].*)\\s*<-(.*)", "\\2", definitionLines[pos] )
+
+  trimws(lsOtherLHS)
+}
+
 #' A model object check.
 #' @description Adds a specified identifier id to all stocks and rates on the model m.
 #' @param model A model object.
 #' @param id A number or string to used as ID.
-#' @param birthTermsQ A logical.
+#' @param addToOtherLeftHandSidesQ Should the id added to other left hand side symbols or not?
+#' @param birthsTermQ Should the births term be included or not?
 #' @return A model.
 #' @details The original model object has \code{birthTermsQ} in the closure of \code{model$RHSFunction}.
 #' (For now) it is easier to just have that parameter specified than imply it.
 #' @family Epidemiology Model Modifier functions
 #' @export
-AddModelIdentifier <- function( model, id, birthTermsQ = FALSE ) {
+AddModelIdentifier <- function( model, id, addToOtherLeftHandSidesQ = TRUE, birthsTermQ = FALSE ) {
 
   if( !EpidemiologyFullModelQ(model) ) {
     stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
@@ -51,6 +65,9 @@ AddModelIdentifier <- function( model, id, birthTermsQ = FALSE ) {
 
   ## New model
   newModel <- model
+
+  ## Deparse RHS function
+  lsRHSdefinition <- deparse(model$RHSFunction, control = "all" )
 
   ## Mapping rules
   aNewStocks <- setNames( gsub( "([[:upper:]]+)t", paste0( "\\1_", id, "_t" ), names(model$Stocks) ), names(model$Stocks) )
@@ -70,7 +87,21 @@ AddModelIdentifier <- function( model, id, birthTermsQ = FALSE ) {
 
   }
 
-  aBirthsTerm <- setNames( ifelse( birthTermsQ, "TRUE", "FALSE" ), c("birthsTermQ") )
+  aBirthsTerm <- setNames( ifelse( birthsTermQ, "TRUE", "FALSE" ), c("birthsTermQ") )
+
+  if( addToOtherLeftHandSidesQ ) {
+
+    # pos <- grep( "\\s([[:alpha:]].*)\\s*<-(.*)", lsRHSdefinition )
+    #
+    # lsOtherLHS <- gsub( "(\\s)([[:alpha:]].*)\\s*<-(.*)", "\\2", lsRHSdefinition[pos] )
+    #
+    # lsOtherLHS <- trimws(lsOtherLHS)
+    lsOtherLHS <- GetLeftHandSides( lsRHSdefinition )
+
+    lsOtherLHS <- setdiff( lsOtherLHS, names(model$Stocks) )
+
+    aOtherLHS <- setNames( paste0(lsOtherLHS, "_", id), lsOtherLHS )
+  }
 
   ## Apply rules to stocks, rates, initial conditions, and rate rules
   newModel$Stocks <- setNames( newModel$Stocks, aNewStocks[ names(newModel$Stocks) ] )
@@ -82,23 +113,160 @@ AddModelIdentifier <- function( model, id, birthTermsQ = FALSE ) {
   newModel$RateRules <- setNames( newModel$RateRules, aNewRates[ names(newModel$RateRules) ] )
 
 
-  ## Deparse RHS function and apply mapping rules
-  lsRHS <- deparse(model$RHSFunction, control = "all" )
+  ## Apply mapping rules to deparsed RHS definition
+  lsRHSdefinition <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocks)[[i]], "\\b" ), aNewStocks[[i]], txt ) }, init = lsRHSdefinition, x = 1:length(aNewStocks) )
 
-  lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocks)[[i]], "\\b" ), aNewStocks[[i]], txt ) }, init = lsRHS, x = 1:length(aNewStocks) )
+  lsRHSdefinition <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocksD)[[i]], "\\b" ), aNewStocksD[[i]], txt ) }, init = lsRHSdefinition, x = 1:length(aNewStocksD) )
 
-  lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocksD)[[i]], "\\b" ), aNewStocksD[[i]], txt ) }, init = lsRHS, x = 1:length(aNewStocksD) )
+  lsRHSdefinition <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocks0)[[i]], "\\b" ), aNewStocks0[[i]], txt ) }, init = lsRHSdefinition, x = 1:length(aNewStocks0) )
 
-  lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewStocks0)[[i]], "\\b" ), aNewStocks0[[i]], txt ) }, init = lsRHS, x = 1:length(aNewStocks0) )
+  lsRHSdefinition <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewRates)[[i]], "\\b" ), aNewRates[[i]], txt ) }, init = lsRHSdefinition, x = 1:length(aNewRates) )
 
-  lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aNewRates)[[i]], "\\b" ), aNewRates[[i]], txt ) }, init = lsRHS, x = 1:length(aNewRates) )
+  lsRHSdefinition <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aBirthsTerm)[[i]], "\\b" ), aBirthsTerm[[i]], txt ) }, init = lsRHSdefinition, x = 1:length(aBirthsTerm) )
 
-  lsRHS <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aBirthsTerm)[[i]], "\\b" ), aBirthsTerm[[i]], txt ) }, init = lsRHS, x = 1:length(aBirthsTerm) )
+  if( addToOtherLeftHandSidesQ ) {
+    lsRHSdefinition <- Reduce( function( txt, i ) { gsub( paste0( "\\b", names(aOtherLHS)[[i]], "\\b" ), aOtherLHS[[i]], txt ) }, init = lsRHSdefinition, x = 1:length(aOtherLHS) )
+  }
 
-  newModel$RHSFunction <- eval( parse( text = lsRHS ) )
+  newModel$RHSFunction <- eval( parse( text = lsRHSdefinition ) )
 
   ## Result
   newModel
+}
+
+
+##===========================================================
+## TakeRHSFunctionBodyLines
+##===========================================================
+
+#' Take RHSFunction body lines.
+#' @description Takes RHSFunction body lines of a model.
+#' @param model A model object.
+#' @return A character vector
+#' @family Epidemiology Model Modifier functions
+#' @export
+TakeRHSFunctionBodyLines <- function( model ) {
+
+  if( !EpidemiologyModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  ## Deparse RHS function
+  lsRHSdefinition <- deparse(model$RHSFunction, control = "all" )
+
+  ## Determine start and end positions
+  posStart <- grep( "^function", lsRHSdefinition )
+  posEnd <- grep( "\\s+return", lsRHSdefinition )
+
+  assertthat::assert_that( posStart == 1 )
+  assertthat::assert_that( grepl( "\\s+\\{", lsRHSdefinition[[posStart+2]] ) )
+
+  lsRHSdefinition[ (posStart+3) : (posEnd-1) ]
+}
+
+
+##===========================================================
+## JoinModels
+##===========================================================
+
+#' Join two models.
+#' @description Makes a multi-site model with the stocks, rates, and equations
+#' of arguments joined.
+#' @param model1 A model object.
+#' @param model2 A model object.
+#' @return A model object.
+#' @family Epidemiology Model Modifier functions
+#' @export
+JoinModels <- function( model1, model2 ) {
+
+  if( !EpidemiologyModelQ(model1) ) {
+    stop( "The argument model1 is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( !EpidemiologyModelQ(model2) ) {
+    stop( "The argument model2 is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( length( intersect( names(model1$Stocks), names(model2$Stocks) ) ) > 0 ) {
+    stop( "The stocks of the two models overlap.", call. = TRUE )
+  }
+
+  if( EpidemiologyFullModelQ(model1) || EpidemiologyFullModelQ(model2) ) {
+    newModel <- SIRModel( initialConditionsQ = TRUE, rateRulesQ = TRUE )
+  } else {
+    newModel <- SIRModel( initialConditionsQ = FALSE, rateRulesQ = FALSE)
+  }
+
+  ## Stocks & rates
+  newModel$Stocks <- c( model1$Stocks, model2$Stocks )
+
+  newModel$Rates <- c( model1$Rates, model2$Rates )
+
+  ## RHS functions
+  lsRHSFuncLines1 <- TakeRHSFunctionBodyLines(model1)
+
+  lsRHSFuncLines2 <- TakeRHSFunctionBodyLines(model2)
+
+  lsStartLines <-
+    c("function( time, state, parameters ) {",
+      "          with(as.list( c( state, parameters ) ),",
+      "               {")
+
+  lsEndLines <-
+    c( "               }",
+       "          )",
+       "        }")
+
+  returnStockNames1 <- names(model1$Stocks)
+  returnStockNames1 <-paste0( "d", returnStockNames1 )
+  returnStockNames1 <- returnStockNames1[ returnStockNames1 %in% GetLeftHandSides( lsRHSFuncLines1 ) ]
+
+  returnStockNames2 <- names(model2$Stocks)
+  returnStockNames2 <-paste0( "d", returnStockNames2 )
+  returnStockNames2 <- returnStockNames2[ returnStockNames2 %in% GetLeftHandSides( lsRHSFuncLines2 ) ]
+
+
+  lsReturnLine <- paste( c( returnStockNames1, returnStockNames2 ), collapse = ", " )
+  lsReturnLine <- paste( "                 return( list( c( ", lsReturnLine, " ) ) )" )
+
+  newModel$RHSFunction <- eval( parse( text = c( lsStartLines, lsRHSFuncLines1, lsRHSFuncLines2, lsReturnLine, lsEndLines ) ) )
+
+  ## Initial conditions and rate rules
+  if( EpidemiologyFullModelQ(newModel) ) {
+
+    newModel$InitialConditions <- c( model1$InitialConditions, model2$InitialConditions )
+
+    newModel$RateRules <- c( model1$RateRules, model2$RateRules )
+
+  }
+
+  #Result
+  newModel
+}
+
+
+##===========================================================
+## MakeCoreMultiSiteModel
+##===========================================================
+
+#' Make core multi-site model.
+#' @description Makes core multi-site model.
+#' @param model A model object.
+#' @param cellIdentifiers A a character vector of cell identifiers.
+#' @return A character vector
+#' @family Epidemiology Model Query functions
+#' @export
+MakeCoreMultiSiteModel <- function( model, cellIdentifiers ) {
+
+  if( !EpidemiologyModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( ! is.character(cellIdentifiers) ) {
+    stop( "The argument cellIdentifiers is expected to be a character vector.", call. = TRUE )
+  }
+
+
 }
 
 
