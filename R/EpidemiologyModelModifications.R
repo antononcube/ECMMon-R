48387a5eda924196c04d0ff9dd0f54490a51b163
antonocube <antononcube@gmail.com>
2020-05-18 11:51:40 -0400

    First version of multi-site model scaling. Added non-generated man pages.

--- R/EpidemiologyModelModifications.R
+++ R/EpidemiologyModelModifications.R
@@ -26,7 +26,102 @@ NULL
 
 
 ##===========================================================
-## AddModelIdentifier
+## SparseMatrixQ
+##===========================================================
+
+SparseMatrixQ <- function (x)
+{
+  sum(c("dgCMatrix", "dgRMatrix", "dgTMatrix") %in% class(x)) > 0
+}
+
+
+##===========================================================
+## GetStocks
+##===========================================================
+
+#' Get stock symbols.
+#' @description Gets the stock symbols that have descriptions matching specified patterns.
+#' @param model A model object.
+#' @param patterns A a character vector of patterns to be applied to the descriptions of model's stocks.
+#' @return A character vector
+#' @family Epidemiology Model Query functions
+#' @export
+GetStocks <- function( model, patterns ) {
+
+  if( !EpidemiologyModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( ! is.character(patterns) ) {
+    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  }
+
+  lsAll <- model$Stocks
+
+  lsFocus <- purrr::map( patterns, function(ss) { grep( pattern = ss, x = lsAll, value = TRUE ) } )
+  lsFocus <- as.character( unlist( lsFocus ) )
+
+  names( model$Stocks[ model$Stocks %in% lsFocus  ] )
+
+}
+
+
+##===========================================================
+## GetRates
+##===========================================================
+
+#' Get rate symbols.
+#' @description Gets the rate symbols that have descriptions matching specified patterns.
+#' @param model A model object.
+#' @param patterns A a character vector of patterns to be applied to the descriptions of model's rates.
+#' @return A character vector
+#' @family Epidemiology Model Query functions
+#' @export
+GetRates <- function( model, patterns ) {
+
+  ## This code is very similar / same as GetStocks -- it has to be refactored.
+
+  if( !EpidemiologyModelQ(model) ) {
+    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  }
+
+  if( ! is.character(patterns) ) {
+    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  }
+
+  lsAll <- model$Rates
+
+  lsFocus <- purrr::map( patterns, function(ss) { grep( pattern = ss, x = lsAll, value = TRUE ) } )
+  lsFocus <- as.character( unlist( lsFocus ) )
+
+  names( model$Rates[ model$Rates %in% lsFocus  ] )
+
+}
+
+
+##===========================================================
+## GrepDefinitions
+##===========================================================
+
+GrepDefinitions <- function( definitionLines, patterns, ... ) {
+
+  if( ! is.character(definitionLines) ) {
+    stop( "The argument definitionLines is expected to be a character vector.", call. = TRUE )
+  }
+
+  if( ! is.character(patterns) ) {
+    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  }
+
+  res <- purrr::map( patterns, function(x) grep( x, definitionLines, ... ) )
+  names(res) <- patterns
+
+  res
+}
+
+
+##===========================================================
+## GetLeftHandSides
 ##===========================================================
 
 GetLeftHandSides <- function( definitionLines ) {
@@ -41,6 +136,11 @@ GetLeftHandSides <- function( definitionLines ) {
 
   trimws(lsOtherLHS)
 }
+
+
+##===========================================================
+## AddModelIdentifier
+##===========================================================
 
 #' A model object check.
 #' @description Adds a specified identifier id to all stocks and rates on the model m.
@@ -233,11 +333,11 @@ JoinModels <- function( model1, model2 ) {
   } else {
 
     returnStockNames1 <- names(model1$Stocks)
-    returnStockNames1 <-paste0( "d", returnStockNames1 )
+    returnStockNames1 <- paste0( "d", returnStockNames1 )
     returnStockNames1 <- returnStockNames1[ returnStockNames1 %in% GetLeftHandSides( lsRHSFuncLines1 ) ]
 
     returnStockNames2 <- names(model2$Stocks)
-    returnStockNames2 <-paste0( "d", returnStockNames2 )
+    returnStockNames2 <- paste0( "d", returnStockNames2 )
     returnStockNames2 <- returnStockNames2[ returnStockNames2 %in% GetLeftHandSides( lsRHSFuncLines2 ) ]
 
 
@@ -262,8 +362,8 @@ JoinModels <- function( model1, model2 ) {
 #' @description Makes core multi-site model.
 #' @param model A model object.
 #' @param cellIdentifiers A a character vector of cell identifiers.
-#' @return A character vector
-#' @family Epidemiology Model Query functions
+#' @return A model object
+#' @family Epidemiology Model Modification functions
 #' @export
 MakeCoreMultiSiteModel <- function( model, cellIdentifiers ) {
 
@@ -282,64 +382,153 @@ MakeCoreMultiSiteModel <- function( model, cellIdentifiers ) {
 
 
 ##===========================================================
-## GetStocks
+## MakeMigrationEquationsEquations
 ##===========================================================
 
-#' Get stock symbols.
-#' @description Gets the stock symbols that have descriptions matching specified patterns.
-#' @param model A model object.
-#' @param patterns A a character vector of patterns to be applied to the descriptions of model's stocks.
+#' Make migration equations.
+#' @description Makes the migration (supplemental) equations with the specified matrix and stocks.
+#' @param data A matrix or a data frame with columns \code{c('i', 'j', 'x')}.
+#' @param migratingStocks A a character vector with stocks.
+#' @param totalStock A string
+#' @param nLeadingWhitespace Number of leading whitespace characters for each equation.
 #' @return A character vector
-#' @family Epidemiology Model Query functions
+#' @family Epidemiology Model Modifier functions
 #' @export
-GetStocks <- function( model, patterns ) {
+MakeMigrationEquations <- function( data, migratingStocks, totalStock = "TPt", nLeadingWhitespace = 8 ) {
 
-  if( !EpidemiologyModelQ(model) ) {
-    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  if( !( is.matrix(data) ||
+         SparseMatrixQ(data) ||
+         is.data.frame(data) && sum( c('i', 'j', 'x') %in% colnames(data) ) == 3 ) ) {
+    stop( "The argument data is expected to be a matrix, a sparse matrix, or a (matrix long form) data frame with columns c('i', 'j', 'x') .", call. = TRUE )
   }
 
-  if( ! is.character(patterns) ) {
-    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  if( ! is.character(totalStock) && length(totalStock) == 1 ) {
+    stop( "The argument totalStock is expected to be a string.", call. = TRUE )
   }
 
-  lsAll <- model$Stocks
+  if( ! is.character(migratingStocks) ) {
+    stop( "The argument migratingStocks is expected to be a character vector.", call. = TRUE )
+  }
 
-  lsFocus <- purrr::map( patterns, function(ss) { grep( pattern = ss, x = lsAll, value = TRUE ) } )
-  lsFocus <- as.character( unlist( lsFocus ) )
+  if( !is.data.frame(data) ) {
+    data <- Matrix::summary( as( data, "dgCMatrix") )
+  }
 
-  names( model$Stocks[ model$Stocks %in% lsFocus  ] )
+  ## Leading white space
+  leadingWhitespace <- paste0( rep_len( x = " ", length.out = nLeadingWhitespace ), collapse = "" )
 
+  ## Remove the last character "t" if present
+  totalStock <- gsub( "t$", "", totalStock)
+  migratingStocks <- gsub( "t$", "", migratingStocks)
+
+  ## Main loop
+  lsRes <-
+    purrr::map( migratingStocks, function(mStock) {
+
+      ## Equations for stocks (people) getting out
+      lsEqsOut <-
+        purrr::map( split( data, data$i ), function(dfX) {
+
+          lhs <- paste0( "d", mStock, "_", dfX$i[[1]], "_t" )
+
+          terms <- purrr::map2_chr( dfX$j, dfX$x, function(j,x) paste0( x, " / ", totalStock, "_", dfX$i[[1]], "_0", " * ", mStock, "_", dfX$i[[1]], "_t" ) )
+
+          paste( lhs, "<-", lhs, "-", paste( terms, collapse = " - " ) )
+        })
+
+      ## Equations for stocks (people) comming in
+      lsEqsIn <-
+        purrr::map( split( data, data$j ), function(dfX) {
+
+          lhs <- paste0( "d", mStock, "_", dfX$j[[1]], "_t" )
+
+          terms <- purrr::map2_chr( dfX$i, dfX$x, function(i,x) paste0( x, " / ", totalStock, "_", i, "_0", " * ", mStock, "_", i, "_t" ) )
+
+          paste( lhs, "<-", lhs, "+", paste( terms, collapse = " + " ) )
+        })
+
+      ## Join equations
+      lsEqsAll <- c( setNames(lsEqsOut, NULL), setNames(lsEqsIn, NULL) )
+
+      ## Put leading whitespace
+      purrr::map( lsEqsAll, function(x) paste0( leadingWhitespace, x ) )
+    })
+
+  names(lsRes) <- migratingStocks
+
+  lsRes
 }
 
 
 ##===========================================================
-## GetRates
+## To site compartmental model
 ##===========================================================
 
-#' Get rate symbols.
-#' @description Gets the rate symbols that have descriptions matching specified patterns.
-#' @param model A model object.
-#' @param patterns A a character vector of patterns to be applied to the descriptions of model's rates.
+#' Crate site compartmental model
+#' @description Makes the migration (supplemental) equations with the specified matrix and stocks.
+#' @param model A (single-site) model object.
+#' @param mat A matrix.
+#' @param migratingStocks A a character vector with stocks (names.)
+#' @param cellIdentifiers A character vector with cell identifiers.
+#' If NULL then \code{unique(c(rownames(mat), colnames(mat)))} is used.
+#' @param ... Additional arguments for \code{MakeMigrationEquations}.
 #' @return A character vector
-#' @family Epidemiology Model Query functions
+#' @family Epidemiology Model Modifier functions
 #' @export
-GetRates <- function( model, patterns ) {
+ToSiteCompartmentalModel <- function( model, mat, migratingStocks = NULL, cellIdentifiers = NULL, ... ) {
 
-  ## This code is very similar / same as GetStocks -- it has to be refactored.
-
-  if( !EpidemiologyModelQ(model) ) {
-    stop( "The argument model is expected to be an epidemiology model object.", call. = TRUE )
+  if( !( is.matrix(mat) || SparseMatrixQ(mat) ) ) {
+    # stop( "The argument data is expected to be a matrix, a sparse matrix, or a (matrix long form) data frame with columns c('i', 'j', 'x') .", call. = TRUE )
+    stop( "The argument data is expected to be a matrix, a sparse matrix.", call. = TRUE )
   }
 
-  if( ! is.character(patterns) ) {
-    stop( "The argument patterns is expected to be a character vector.", call. = TRUE )
+  ## Dealing with matrix names
+  if( is.null(rownames(mat)) && is.null(colnames(mat)) ) {
+
+    rownames(mat) <- seq(1, nrow(mat))
+    colnames(mat) <- seq(1, ncol(mat))
+
+  } else if ( is.null(rownames(mat))  ) {
+
+    warning( "Assigning automatic rownames.", call. = TRUE )
+    rownames(mat) <- seq(1, nrow(mat))
+
+  } else if ( is.null(colnames(mat))  ) {
+
+    warning( "Assigning automatic colnames", call. = TRUE )
+    colnames(mat) <- seq(1, ncol(mat))
+
   }
 
-  lsAll <- model$Rates
+  if( is.null(cellIdentifiers) ) {
+    cellIdentifiers <- unique( c(rownames(mat), colnames(mat)) )
+  }
 
-  lsFocus <- purrr::map( patterns, function(ss) { grep( pattern = ss, x = lsAll, value = TRUE ) } )
-  lsFocus <- as.character( unlist( lsFocus ) )
+  if( is.null(migratingStocks) ) {
+    migratingStocks <- c( "SPt", "EPt", "RPt", "INSPt", "IPt" )
+    migratingStocks <- intersect( migratingStocks, names(model$Stocks) )
+  }
 
-  names( model$Rates[ model$Rates %in% lsFocus  ] )
+  ## Core mult-site model extension
+  modelMultiSite <- MakeCoreMultiSiteModel( model = model, cellIdentifiers = cellIdentifiers )
 
+  ## Derive migration terms equations
+  lsMigrationEquations <- MakeMigrationEquations( data = mat, migratingStocks = migratingStocks, ... )
+  lsMigrationEquations <- do.call(c, lsMigrationEquations)
+  #lsMigrationEquations <- paste( lsMigrationEquations, ";" )
+
+  ## Add migration terms equations
+  lsRHSLines <- deparse(modelMultiSite$RHSFunction, control = "all" )
+
+  pos <- GrepDefinitions( lsRHSLines, patterns = "^\\s*return" )
+
+  assertthat::assert_that( length(pos) == 1 )
+  pos <- pos[[1]]
+
+  lsRHSLines <- c( lsRHSLines[1:(pos-1)], "", lsMigrationEquations, "", lsRHSLines[pos:length(lsRHSLines)] )
+
+  modelMultiSite$RHSFunction <- eval( parse( text = lsRHSLines ) )
+
+  ## Result
+  modelMultiSite
 }
